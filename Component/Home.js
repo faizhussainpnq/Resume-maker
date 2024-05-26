1fc3ba9cf9a5c1277b9d9b690526bc9dfac046bf
Faiz Husssain <140240291+faizhussainpnq@users.noreply.github.com>
2024-05-26 19:32:44 +0530

    Update Home.js

--- Component/Home.js
+++ Component/Home.js
@@ -8,11 +8,15 @@ export default function Home() {
   return (
     <>
  {/* <Navbar/> */}
+  {/* Header section container */}
 <div className="header">
+   {/* Header image */}
    <img src={resumehomeSvg} alt="" id='Headerimage'/>
+  {/* Main heading with spans for emphasizing 'Resume' */}
    <h1 className='thought' >Online <span>Resume</span> Maker <br /> Build a Professional <span>Resume</span> Now</h1>
    </div>
    <div className="create">
+   {/* Button that links to the resume builder page */}
    <button><Link to="/resumebuilder" style={{textDecoration:"none"}} ><h6 id='creating' >CREATE YOUR RESUME</h6></Link></button>
    </div>
 
