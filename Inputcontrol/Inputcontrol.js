d00c2e61a4b3c9c8e6098950c3dff570855156b2
Faiz Husssain <140240291+faizhussainpnq@users.noreply.github.com>
2024-05-29 13:41:27 +0530

    Inputcontrol.js

--- Inputcontrol/Inputcontrol.js
+++ Inputcontrol/Inputcontrol.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../Inputcontrol/Inputcontrol.css"
 
+// This is a reusable input control component that accepts a label and other props.
 export default function Inputcontrol({label,...props}) {
   return (
     <>
